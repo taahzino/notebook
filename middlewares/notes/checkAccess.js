f6fae3c4450b59d6f11398a75e22fc687dfd6dcd
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2021-09-23 22:21:30 +0600

    <refactor>: checkAccess middleware

--- middlewares/notes/checkAccess.js
+++ middlewares/notes/checkAccess.js
@@ -1,16 +1,8 @@
 const checkAccess = (req, res, next) => {
     const id = req.params.id.length > 0 ? req.params.id : false;
     const { notes } = res.locals.user;
-    if (notes.length > 0) {
-        if (notes.includes(id)) {
-            next();
-        } else {
-            res.status(400).json({
-                errors: {
-                    common: { msg: 'Bad Request' },
-                },
-            });
-        }
+    if (typeof notes === 'object' && notes instanceof Array && notes.includes(id)) {
+        next();
     } else {
         res.status(400).json({
             errors: {
