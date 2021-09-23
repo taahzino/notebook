03bed9027b56ce290ede35239ed1e05b6d06a132
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2021-09-23 22:47:40 +0600

    <fix>: Check note id param for checkAccess middleware - note

--- middlewares/notes/checkAccess.js
+++ middlewares/notes/checkAccess.js
@@ -1,12 +1,20 @@
 const checkAccess = (req, res, next) => {
     const id = req.params.id.length > 0 ? req.params.id : false;
     const { notes } = res.locals.user;
-    if (typeof notes === 'object' && notes instanceof Array && notes.includes(id)) {
-        next();
+    if (id) {
+        if (typeof notes === 'object' && notes instanceof Array && notes.includes(id)) {
+            next();
+        } else {
+            res.status(403).json({
+                errors: {
+                    common: { msg: 'You do not have access to this resource!' },
+                },
+            });
+        }
     } else {
-        res.status(403).json({
+        res.status(400).json({
             errors: {
-                common: { msg: 'You do not have access to this resource!' },
+                common: { msg: 'Bad request' },
             },
         });
     }
